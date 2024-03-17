2fe67ec3fd0f67d85eb43a7164f08894bc1caa9a
ale94 <alejandrorua111@gmail.com>
2024-03-17 12:14:37 -0300

    Add bootstrap al proyecto

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -1,15 +1,69 @@
 import { getInovice } from "./services/getInovice";
 
 export const InvoiceApp = () => {
-  const invoice = getInovice();
+  const { id, name, client, company, items } = getInovice();
+  const { name: nameClient, lastName, address } = client;
+  const { country, city, street, number } = address;
 
   return (
     <>
-      <h1>Ejemplo Factura</h1>
-      <ul>
-        <li>Id: {invoice.id}</li>
-        <li>Name: {invoice.name}</li>
-      </ul>
+      <div className="container">
+        <div className="card shadow my-3">
+          <div className="card-header">Ejemplo Factura</div>
+
+          <div className="card-body">
+            <ul className="list-group">
+              <li className="list-group-item">Id: {id}</li>
+              <li className="list-group-item">Name: {name}</li>
+            </ul>
+
+            <div className="row my-3">
+              <div className="col">
+                <h3>Datos del cliente</h3>
+                <ul className="list-group">
+                  <li className="list-group-item active">
+                    {nameClient} {lastName}
+                  </li>
+                  <li className="list-group-item">
+                    {country} / {city}
+                  </li>
+                  <li className="list-group-item">
+                    {street} {number}
+                  </li>
+                </ul>
+              </div>
+              <div className="col">
+                <h3>Datos de la empresa</h3>
+                <ul className="list-group">
+                  <li className="list-group-item active">{company.name}</li>
+                  <li className="list-group-item">{company.fiscalNumber}</li>
+                </ul>
+              </div>
+            </div>
+            <h4>Productos de la factura</h4>
+            <table className="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>Producto</th>
+                  <th>Precio</th>
+                  <th>Cantidad</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map(({ id, product, price, quantity }) => {
+                  return (
+                    <tr key={id}>
+                      <td>{product}</td>
+                      <td>{price}</td>
+                      <td>{quantity}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/data/invoice.js
+++ src/data/invoice.js
@@ -17,16 +17,19 @@ export const invoice = {
   },
   items: [
     {
+      id: 1,
       product: "Cpu Intel i7",
       price: 499,
       quantity: 0,
     },
     {
+      id: 2,
       product: "Corsair Keyboard Mecanico",
       price: 150,
       quantity: 0,
     },
     {
+      id: 3,
       product: "Monitor Asus",
       price: 350,
       quantity: 0,
